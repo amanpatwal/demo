50624f1bdfc05131af282f78a5d3aa3f7a3abdbe
amanpatwal <amansinghpatwal2003@gmail.com>
2025-02-22 15:46:30 +0530

    first

---
+++ contact/src/app/page.js
@@ -0,0 +1,27 @@
+import { MapPin } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-100 h-100">
+        <div className="grid md:grid-cols-2 ">
+          <div>
+            <h1 className="text-[#E62E2D] font-bold ">// CONTACT US</h1>
+            <p className="text-5xl text-[#171B2A] font-bold">
+              HAVE LEGAL QUERIES? LEAVE THEM TO US.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa
+              Aliquam in hendrerit urna
+            </p>
+            <div className="flex">
+              <MapPin />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
